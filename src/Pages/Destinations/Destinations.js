c8b314b758a62b9a43ff32e002d1b94155cef279
UmmeNafisa <pappanafisa@gmail.comm>
2021-10-30 19:15:50 +0600

    destination complete

--- src/Pages/Destinations/Destinations.js
+++ src/Pages/Destinations/Destinations.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import packageImg from '../../Images/wave-design.jpg'
 import './Destination.css'
 import desImg1 from "../../Images/destination/destination-img1.jpg"
@@ -13,33 +13,32 @@ const Destinations = () => {
             <Container className="pb-5">
                 <div>
                     <h3 className="all-headers text-color fw-bolder pt-5 text-align"> <span className="fw-bolder text-color">|</span> Amazing Destination</h3>
-                    <h2 className="text-align mb-3">Choose The Destination Just Right For Your Vacation </h2>
+                    <h2 className="text-align mb-3 stylish-front">Choose The Destination Just Right For Your Vacation </h2>
                 </div>
-                <Row xs={1} md={1} lg={2} className="g-4">
+                <Row xs={1} md={1} lg={2} className="g-4 text-align">
                     <Col>
                         <Row xs={1} md={1} lg={1} className="g-4">
+
                             <Col>
-                                <Card.Img src={desImg1} alt="Card image" />
-                                <Card.ImgOverlay>
-                                    <Button className="btn-all"> Greece </Button>
-                                </Card.ImgOverlay>
+                                <img src={desImg1} alt="" className="img-fluid rounded-3 des-images" />
+                                <Button className="btn-all px-5 dest-title stylish-front fs-4 mb-3"> Greece </Button>
                             </Col>
 
                         </Row>
                         <Row xs={1} md={1} lg={2} className="g-4">
                             <Col>
                                 <img src={desImg2} alt="" className="img-fluid rounded-3" />
-                                <Button className="btn-all"> Italy </Button>
+                                <Button className="btn-all px-5 dest-title stylish-front fs-4 mb-3"> Italy </Button>
                             </Col>
                             <Col>
                                 <img src={desImg3} alt="" className="img-fluid des-img-3 rounded-3" />
-                                <Button className="btn-all"> Switzerland </Button>
+                                <Button className="btn-all px-5 dest-title stylish-front fs-4 mb-3"> Switzerland </Button>
                             </Col>
                         </Row>
                     </Col>
                     <Col>
                         <img src={desImg4} alt="" className=" h-100 w-100 rounded-3" />
-                        <Button className="btn-all"> India </Button>
+                        <Button className="btn-all px-5 dest-title stylish-front fs-4 mb-3"> India </Button>
                     </Col>
                 </Row>
             </Container>
--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -20,7 +20,7 @@ const About = () => {
                     </div>
                     <div className="pt-2 w-75 about-us">
                         <h3 className="all-headers text-color fw-bolder pt-5 text-align"> <span className="fw-bolder text-color">|</span> About us</h3>
-                        <h2 className="text-align mb-3">Plan Your Trip with Us </h2>
+                        <h2 className="text-align mb-3 stylish-front">Plan Your Trip with Us </h2>
                         <p className="text-muted header-front text-align">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic. Italic Mountains, she had a last view back on the skyline</p>
                         <Link to=""> <Button className="btn-all fw-bold px-3 py-2 ms-0"> LEARN MORE </Button> </Link>
                     </div>
